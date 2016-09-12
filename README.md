b9857cb7306b674de52d5dadb29cab77071e565d
dmodena <d.modena@live.com>
2016-09-12 15:00:49 -0300

    Filtrando palavras e limitando texto.

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 BOW - Bag of Words
 ===================
-Ao executar este script, o sistema irá pedir uma entrada de texto. Digite ou cole o texto desejado e pressione ENTER.
+O texto usado pelo sistema encontra-se no arquivo *text.txt*.
 
 O sistema irá varrer o texto, passando todas as palavras para letras minúsculas, removendo os caracteres não alfanuméricos e separando as palavras por espaços. A partir disso, uma lista de palavras será criada.
 
 A partir da lista de palavras, um dicionário será criado para contar a quantidade de ocorrências de cada palavra no texto.
+
+Palavras não relevantes serão filtradas do texto, e a nova lista incluirá até 1000 palavras, entre as mais relevantes.
 
 Por fim, o sistema irá imprimir cada palavra com um valor de 0 a 1, de acordo com a quantidade de vezes que a palavra aparece no texto (sua relevância).
--- bow.py
+++ bow.py
@@ -2,6 +2,7 @@
 import operator
 import codecs
 import re
+from collections import Counter
 
 # Retorna a quantidade de pontos para formatação
 def pts(number):
@@ -22,29 +23,35 @@ fd.close()
 
 # Criando lista a partir do texto, ignorando capitalização
 #  e caracteres especiais
-words = [re.sub('\W', '', block) for block in text.lower().split(" ")]
+all_words = re.findall(r'\w+', text.lower())
+
+# Stopwords
+stopwords = ['a', 'ante', 'as', 'até', 'após', 'com', 'contra', 'de', 'desde', 'o', 'os', 'para', 'perante', 'por', 'sem', 'sob', 'sobre', 'trás', 'um', 'uma', 'uns', 'umas']
+
+# Removendo stopwords
+words = [w for w in all_words if w not in stopwords]
+
+# Contador de palavras
+word_count = Counter(words)
+total_words = len(words)
 
 # Criando dicionário de palavras
 indices = {}
-
-# Adicionando valores de acordo com quantidade de vezes
-#  em que a palavra aparece
-for word in words:
-    if word in indices:
-        indices[word] += 1
-    else:
-        indices[word] = 1
-
-# Recalculando valores de acordo com total de palavras
-total_words = len(words)
-for word in indices:
-    indices[word] /= total_words
+for w in word_count:
+    indices[w] = word_count[w] / total_words
 
 # Ordenando resultados
 sorted_indices = sorted(indices.items(), key=operator.itemgetter(1), reverse=True)
 
+# Removendo palavras menos usadas
+sorted_indices_size = len(sorted_indices)
+limit_size = sorted_indices_size - int(round(sorted_indices_size / 10))
+
+# Limitando tamanho da list
+filtered_list = sorted_indices[0:1000] if limit_size > 1000 else sorted_indices[0:limit_size]
+
 # Imprimindo resultados
 print()
-imprimir(sorted_indices)
+imprimir(filtered_list)
 print()
-print(str(total_words) + " palavras distintas.")
+print(str(len(filtered_list)) + " palavras distintas.")
